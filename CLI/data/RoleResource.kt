5fe0d6daf9c17e0db008fd7ace5f4b703d1176bb
Администратор <slegionv@yandex.ru>
2020-10-30 16:16:39 +0700

    Пункт 8.2. Созданы классы обертки User, RoleResource и Activity для работы с данными.

---
+++ CLI/data/RoleResource.kt
@@ -0,0 +1,8 @@
+package data
+
+data class RoleResource(
+    val id: Int,
+    val role: String,
+    val resource: String,
+    val idUser: Int
+)
---
+++ CLI/data/User.kt
@@ -0,0 +1,7 @@
+package data
+
+data class User(
+    val id: Int,
+    val login: String,
+    val hashPassword: String
+)
