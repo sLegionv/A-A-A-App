bf2d4d39f385782ea8a633c553ae3e06e92d2bae
Администратор <slegionv@yandex.ru>
2020-10-30 16:10:50 +0700

    Пункт 7.2 и 7.3 реализованы в методе run

--- CLI/services/HandlerCLI.kt
+++ CLI/services/HandlerCLI.kt
@@ -1,4 +1,23 @@
 package services
 
-class HandlerCLI {
+class HandlerCLI() {
+    private val commands = arrayOf("-login", "-pass", "-res", "-role", "-ds", "-de", "-vol")
+
+    fun run(args: Array<String>): MutableMap<String, String?> {
+        val commandsArgs: MutableMap<String, String?> = commands.map { it to null }.toMap().toMutableMap()
+        val sizeArgs = args.size
+        when {
+            args.isEmpty() -> terminate()
+            args.contains("-h") -> terminate()
+            sizeArgs !in arrayOf(4, 8, 14) -> terminate()
+            sizeArgs != args.distinct().size -> terminate()
+        }
+        for (i in 0 until sizeArgs step 2) {
+            val arg = args[i]
+            if (arg !in commands)
+                terminate()
+            commandsArgs[arg] = args[i + 1]
+        }
+        return commandsArgs
+    }
 }
