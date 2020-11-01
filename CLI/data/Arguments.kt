12f6a7c6c0070f0dfc42c95ea9841ca27a10ab3c
legion <slegionv@yandex.ru>
2020-11-01 17:01:23 +0300

    Data class Arguments внесен в HandlerCLI

--- CLI/data/Arguments.kt
+++ CLI/data/Arguments.kt
@@ -1,11 +1,17 @@
 package data
 
 data class Arguments(
-    val h: Boolean,
-    val login: String?,
-    val pass: String?,
-    val role: String?,
-    val res: String?,
-    val ds: String?,
-    val de: String?,
-    val vol: String?)
+    var h: Boolean=false,
+    var login: String?=null,
+    var pass: String?=null,
+    var role: String?=null,
+    var res: String?=null,
+    var ds: String?=null,
+    var de: String?=null,
+    var vol: String?=null) {
+
+    fun hasHelp() = h
+
+    fun isEmpty() = (login == null && pass == null && role == null &&
+            res == null && ds == null && de == null && vol == null)
+}
--- CLI/services/HandlerCLI.kt
+++ CLI/services/HandlerCLI.kt
@@ -1,28 +1,31 @@
 package services
 import data.Arguments
+import java.security.KeyStore
 
 class HandlerCLI() {
-    private val commands = arrayOf("-h", "-login", "-pass", "-res", "-role", "-ds", "-de", "-vol")
 
-    fun run(args: Array<String>): Arguments {
+    fun parse(args: Array<String>): Arguments {
         val arguments = Arguments()
         val sizeArgs = args.size
-        when {
-            args.isEmpty() -> terminate()
-            args.contains("-h") -> terminate()
-            sizeArgs !in arrayOf(4, 8, 14) -> terminate()
-            sizeArgs != args.distinct().size -> terminate()
-        }
+        if (isInvalidSize(sizeArgs)) return arguments
         for (i in 0 until sizeArgs step 2) {
-            val arg = args[i]
-            if (arg !in commands)
-                terminate()
-            commandsArgs[arg] = args[i + 1]
+            fillField(arguments, args[i], args[i + 1])
         }
-        return commandsArgs
+        return arguments
+    }
+    private fun isInvalidSize(sizeArgs: Int): Boolean = (sizeArgs !in arrayOf(0, 1, 4, 8, 14))
+
+    private fun fillField(arguments: Arguments, arg: String, argValue: String) {
+        when (arg) {
+            "-h" -> arguments.h = true
+            "-login" -> arguments.login = argValue
+            "-pass" -> arguments.pass = argValue
+            "-res" -> arguments.res = argValue
+            "-role" -> arguments.role = argValue
+            "-ds" -> arguments.ds = argValue
+            "-de" -> arguments.de = argValue
+            "-vol" -> arguments.vol = argValue
+        }
     }
 
-    fun checkAmountParams(x: Any): Boolean {
-        
-    }
 }
