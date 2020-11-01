40c6dce961102263f59b6a37b4b1fec1e242d95c
peryza <51073553+peryza@users.noreply.github.com>
2020-11-02 03:21:21 +0700

    Обновление App.kt

--- CLI/App.kt
+++ CLI/App.kt
@@ -1,7 +1,21 @@
-import data.ExitCodes.*
+import data.ExitCodes.Help
+import data.ExitCodes.Success
+import services.HandlerCLI
 
 class App {
     fun run(args: Array<String>): Int {
+
+        val handlerCLI = HandlerCLI()
+        val arguments = handlerCLI.parse(args)
+
+        when {
+            arguments.hasHelp() -> return Help.exitCode
+            arguments.isEmpty() -> return Help.exitCode
+        }
+
+
         return Success.exitCode
     }
+
+
 }
