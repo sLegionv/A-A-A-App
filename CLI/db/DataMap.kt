e2987aa3a79031e8174e4e46cb21bf5b0dd934a2
legion <slegionv@yandex.ru>
2020-11-05 10:40:03 +0300

    Изменение структуры базы.

--- CLI/db/DataMap.kt
+++ CLI/db/DataMap.kt
@@ -1,18 +1,24 @@
 package db
 
-val tableUsers = mutableListOf(
-    mapOf("id" to "1", "login" to "vasya", "pass" to "123", "salt" to ""),
-    mapOf("id" to "2", "login" to "admin", "pass" to "admin", "salt" to ""),
-    mapOf("id" to "3", "login" to "q", "pass" to "ytrewq", "salt" to ""))
+import data.Activity
+import data.RoleResource
+import data.Roles.*
+import data.User
 
-val tableRolesResources = mutableListOf(
-    mapOf("id" to "1", "role" to "A", "res" to "READ", "idUser" to "1"),
-    mapOf("id" to "2", "role" to "A.B.C", "res" to "WRITE", "idUser" to "1"),
-    mapOf("id" to "3", "role" to "A.B", "res" to "EXECUTE", "idUser" to "2"),
-    mapOf("id" to "4", "role" to "B", "res" to "EXECUTE", "idUser" to "3"),
-    mapOf("id" to "5", "role" to "A.B.C", "res" to "READ", "idUser" to "2"),
-    mapOf("id" to "6", "role" to "A.B", "res" to "WRITE", "idUser" to "2"),
-    mapOf("id" to "7", "role" to "A", "res" to "READ", "idUser" to "2")
+val tableUsers = mutableListOf(
+        User(1, "vasya", "7a5a73db77e24a3964fa333fd43be8bb", "bv5PehSMfV11Cd"),
+        User(2, "admin", "b43cfeda0e8e4bd96561535db8a1d377", "QxLUF1bgIAdeQX"),
+        User(3, "q", "4a220600490d41af793cbb5c4494e435", "YYLmfY6IehjZMQ")
 )
 
-val tableActivity: MutableList<Map<String, String>> = mutableListOf()
+val tableRolesResources = mutableListOf(
+        RoleResource(1, READ, "A", 1),
+        RoleResource(2, WRITE, "A.B.C", 1),
+        RoleResource(3, EXECUTE, "A.B", 2),
+        RoleResource(4, EXECUTE, "B", 3),
+        RoleResource(5, READ, "A.B.C", 2),
+        RoleResource(6, WRITE, "A.B", 2),
+        RoleResource(7, READ, "A", 2)
+)
+
+val tableActivity: MutableList<Activity> = mutableListOf()
