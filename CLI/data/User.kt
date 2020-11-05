5c3222286f93b0272a8bee42bb4390c8542a8dbf
legion <slegionv@yandex.ru>
2020-11-05 10:41:16 +0300

    Изменение класса обертки User.

--- CLI/data/User.kt
+++ CLI/data/User.kt
@@ -1,8 +1,11 @@
 package data
 
 data class User(
-        val id: Int,
-        val login: String,
-        val Password: String,
-        val salt: String
-)
+        val id: Long? = null,
+        val login: String? = null,
+        val hashPassword: String? = null,
+        val salt: String? = null
+) {
+    // Проверка на существование пользователя
+    fun isInvalidUser() = id != null
+}
